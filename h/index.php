33cf6d58d8abf6c9d89859bdabff18b470e19077
nawapol4032 <66010914032@msu.ac.th>
2026-02-04 15:20:17 +0700

    a

--- h/index.php
+++ h/index.php
@@ -14,5 +14,26 @@ Password<input type="password" name="apwd" required> <br>
 <button type="submit" name="Submit">LOGIN</button>
 </form>
 
+<?php
+if(isset($_POST['Submit'])) {
+    include_once("connectdb.php");
+    $sql = "SELECT * FROM admin WHERE a_username='{$_POST['auser']}' AND a_password='{$_POST['apwd']}' LIMIT 1";
+    $rs = mysqli_query($conn, $sql);
+    $num = mysqli_num_rows($rs)
+
+    if ($num == 1) {
+        $data = mysqli_fetch_array($rs);
+        $_SESSION['aid'] = $data['a_id'];
+        $_SESSION['aname'] = $data['a_name'];
+        echo "<script>" ;
+        echo "window.location='index2.php';";
+        echo "</script>" ; 
+    } else {
+        echo "script>" ;
+        echo "alert('Username หรือ Paassword ไม่ถูกต้อง');";
+        echo "</script>";
+}
+?>
+
 </body>
 </html>
