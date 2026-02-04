93a1a325e31592c3435d3827aff9f96338d3d28d
nawapol4032 <66010914032@msu.ac.th>
2026-02-04 17:08:13 +0700

    a

--- g/connectdb.php
+++ g/connectdb.php
@@ -1,7 +1,7 @@
 <?php
 		$host = "localhost";
 		$user = "root";
-		$pwd = "1234";
+		$pwd = "";
 		$db = "4032db";
 		$conn = mysqli_connect($host, $user, $pwd, $db) or die ("เชื่อมต่อฐานข้อมูลไม่ได้");
 		mysqli_query($conn, "SET NAMES utf8");
