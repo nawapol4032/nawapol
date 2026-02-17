4ddf131ce11288c81f82202ea37d14d797e97639
nawapol <66010914032@msu.ac.th>
2026-02-17 18:18:13 +0700

    i

--- i/a.php
+++ i/a.php
@@ -6,6 +6,19 @@
 </head>
 
 <body>
-<h1>66010914032 นวพล ชุมพล</h1>
+<h1>งาน i- 66010914032 นวพล ชุมพล</h1>
+
+<?php
+include_once("connectdb_php")
+$sql = "SELECT * FROM 'regions'";
+$rs = mysqli_query($conn, $sql)
+while ($data - mysqli_fetch_arry($rs)){
+    echo $data['r_id']."<br>"
+    echo $data['r_name']."<br>"
+}
+
+mysqli_closer($conn);
+?>
+
 </body>
 </html>
