aaa4a531cb23122db593f615eacbb78b61ebf12c
nawapol <66010914032@msu.ac.th>
2026-02-17 19:48:27 +0700

    i

--- i/a.php
+++ i/a.php
@@ -1,63 +1,58 @@
-<!doctype html>
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-<meta charset="utf-8">
-<title>นวพล ชุมพล(หลุยส์)</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>นวพล ชุมพล(หลุยส์)</title>
 </head>
-
 <body>
 
-<h1>งาน i -- นวพล ชุมพล(หลุยส์)</h1>
+<h1>งาน i นวพล ชุมพล(หลุยส์)</h1>
+
+<?php
+include_once("connectdb.php");
+
+if(isset($_POST['Submit'])){
+    $rname = $_POST['rname'];
+    $sql2 = "INSERT INTO region (r_id, r_name) VALUES (NULL, '{$rname}')";
+    mysqli_query($conn,$sql2) or die ("เพิ่มข้อมูลไม่ได้");
+}
+
+$sql = "SELECT * FROM region";
+$rs = mysqli_query($conn, $sql);
+?>
 
 <form method="post" action="">
     ชื่อภาค <input type="text" name="rname" autofocus required>
     <button type="submit" name="Submit">บันทึก</button>
-</form><br><br>
-
-<?php
-if(isset($_POST['Submit'])) {
-    include_once("connectdb.php");
-    $sql2 = "INSERT INTO regions (r_id, r_name) VALUES (NULL,
-'{$rname}')";
-    mysqli_query($conn, $sql2) or die ("เพิ่มข้อมูลไม่ได้");
-}
-?>
-
-
-
+</form><br>
 
 <table border="1">
     <tr>
         <th>รหัสภาค</th>
         <th>ชื่อภาค</th>
         <th>ลบ</th>
-
     </tr>
+
 <?php
-include_once("connectdb.php");
-$sql = "SELECT * FROM regions";
-$rs = mysqli_query($conn, $sql);
-while ($data = mysqli_fetch_array($rs)) {
+while ($data = mysqli_fetch_array($rs)){
 ?>
     <tr>
-        <td><?php echo $data['r_id']; ?></td>
-        <td><?php echo $data['r_name']; ?></td>
-        <td width="80" align="center"><images/delete.jpg" width="20"></td>
-    </tr> 
-<?php } 
-?>
-</table>
-
+        <td><?php echo $data['r_id'];?></td>
+        <td><?php echo $data['r_name'];?></td>
+        <td width="80" align="center">
+            <a href="delete_region.php?id=<?php echo $data['r_id'];?>" 
+               onclick="return confirm('ยืนยันการลบหรือไม่');">
+               <img src="images/Delete.png" width="20">
+            </a>
+        </td>
+    </tr>
 <?php
+}
 mysqli_close($conn);
 ?>
 
-
-
-
-
-
-
+</table>
 
 </body>
 </html>
