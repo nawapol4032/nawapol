d84c4524c18c117cb87e08ef20fc9d973f920eb9
nawapol <66010914032@msu.ac.th>
2026-02-17 20:41:07 +0700

    i

---
+++ i/delete_regions.php
@@ -0,0 +1,14 @@
+<meta charset="utf-8">
+<?php
+include_once("connectdb.php");
+
+$id = $_GET["id"];
+$sql ="DELETE FROM regions WHERE r_id='$id'";
+mysqli_query($conn,$sql) or die ("เพิ่มข้อมูลไม่ได้");
+
+echo "<script>";
+echo "window.location='a.php';";
+echo "</script>";
+
+
+?>
